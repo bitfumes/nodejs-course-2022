553e4abaf3346d1441b14a9b8bf65eb955510734
sarthaksavvy <sarthak@bitfumes.com>
2022-08-01 00:22:58 +0530

    11 Nodejs course - http handle routes

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 const http = require("http");
 const PORT = 3000;
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(
-    JSON.stringify({
-      data: "Hello World!",
-    })
-  );
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(
+      JSON.stringify({
+        data: "Hello World!",
+      })
+    );
+  }
+
+  if (req.url === "/about") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(
+      JSON.stringify({
+        data: "I am about page",
+      })
+    );
+  }else{
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(
+      JSON.stringify({
+        data: "page not found",
+      })
+    );
+  }
 });
 
 console.log(`server is running at http://localhost:${PORT}`);
