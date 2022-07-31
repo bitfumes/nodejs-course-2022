8c07e42d5be4d4fc35e3a1fbd740e9bf6d8feacf
sarthaksavvy <sarthak@bitfumes.com>
2022-08-01 00:00:32 +0530

    09 Nodejs course - create package.json file

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
-console.log(__filename);
+const http = require("http");
+const PORT = 3000;
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(
+    JSON.stringify({
+      data: "Hello World!",
+    })
+  );
+});
 
-// const Circle = require("./circle");
-// const circle = new Circle();
-// console.log(circle.area(5));
-// console.log(circle.circumference(5));
-
-// const http = require("http");
-
-// const server = http.createServer((req, res) => {
-//   res.writeHead(200, { "Content-Type": "application/json" });
-//   res.end(
-//     JSON.stringify({
-//       data: "Hello World!",
-//     })
-//   );
-// });
-
-// server.listen(3000);
+console.log(`server is running at http://localhost:${PORT}`);
+server.listen(PORT);
