06daf0375ca2cf96bdab4054b19add6a4902a4c3
sarthaksavvy <sarthak@bitfumes.com>
2022-08-01 20:28:59 +0530

    29 Nodejs course - nodejs auth login

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -8,3 +8,18 @@ exports.signup = async (req, res) => {
   const user = await User.create(data);
   res.json({ user });
 };
+
+exports.login = async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    res.status(404).json({ error: "User not found" });
+    return;
+  }
+
+  if (!(await bcrypt.compare(req.body.password, user.password))) {
+    res.status(404).json({ error: "User not found" });
+    return;
+  }
+
+  res.json({ user });
+};
--- routes/auth.js
+++ routes/auth.js
@@ -3,5 +3,6 @@ const authRoutes = express.Router();
 const AuthController = require("../controllers/AuthController");
 
 authRoutes.post("/signup", AuthController.signup);
+authRoutes.post("/login", AuthController.login);
 
 module.exports = authRoutes;
