31b6d13e2a5acdf5153ff6419270568776766949
sarthaksavvy <sarthak@bitfumes.com>
2022-08-01 21:01:50 +0530

    32 Nodejs course - jwt authorization

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,27 @@
-const auth = (req, res, next) => {
-  res.status(401).json({ error: "Unauthorized" });
+const jwt = require("jsonwebtoken");
+const { ObjectId } = require("mongodb");
+const User = require("../models/User");
+
+const auth = async (req, res, next) => {
+  if (
+    req.headers &&
+    req.headers.authorization &&
+    req.headers.authorization.split(" ")[1]
+  ) {
+    const token = req.headers.authorization.split(" ")[1];
+    try {
+      const decode = await jwt.verify(token, "fake-jwt-secret");
+      const user = await User.findOne({ _id: ObjectId(decode.user._id) });
+      if (!user) {
+        res.status(401).json({ error: "Unauthorized" });
+        return;
+      }
+
+      next();
+    } catch (error) {
+      res.status(401).json({ error: "Unauthorized" });
+    }
+  }
 };
 
 module.exports = auth;
