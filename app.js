3e92412005d238aef1f069374630fd713d0af24d
sarthaksavvy <sarthak@bitfumes.com>
2022-08-01 11:23:29 +0530

    14 Nodejs course - write large file

--- app.js
+++ app.js
@@ -13,11 +13,22 @@ const server = http.createServer((req, res) => {
     });
   } else if (req.url === "/about") {
     res.writeHead(200, { "Content-Type": "text/html" });
-    fs.readFile("page/about.html", "utf8", (err, data) => {
+    fs.readFile("temp/test.html", "utf8", (err, data) => {
       if (err) throw err;
       res.write(data);
       res.end();
     });
+  } else if (req.url === "/create-file") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    const data = "<h1>This is test file</h1>";
+    for (let i = 0; i < 100000; i++) {
+      fs.appendFile("temp/test.html", data, (err) => {
+        if (err) throw err;
+      });
+    }
+
+    res.write("file is created");
+    res.end();
   } else {
     res.writeHead(404, { "Content-Type": "text/html" });
     fs.readFile("page/404.html", "utf8", (err, data) => {
