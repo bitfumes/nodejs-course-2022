f22c34c586f9b824aa205849b79aeb83135dd4d3
sarthaksavvy <sarthak@bitfumes.com>
2022-08-01 14:17:13 +0530

    20 Nodejs course - express server html file

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const bookRouter = require("./book");
+const path = require("path");
 
 router.get("/", (req, res) => {
-  res.send("Hello World");
+  res.sendFile(path.join(__dirname, "/../page/home.html"), { name: "sarthak" });
 });
 
 router.use("/book", bookRouter);
 
 router.all("/*", (req, res) => {
-  res.send("Page Not Found");
+  res.sendFile(path.join(__dirname, "/../page/404.html"));
 });
 
 module.exports = router;
